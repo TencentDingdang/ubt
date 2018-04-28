8cd22151ec8e71978e94d817cbab346b56420406
rincliu <rincliu@tencent.com>
2018-04-28 11:12:48 +0800

    添加日志开关；

--- ios/sdk/TVSAccountSDK.framework/Headers/TVSEnvironment.h
+++ ios/sdk/TVSAccountSDK.framework/Headers/TVSEnvironment.h
@@ -11,6 +11,7 @@
 
 /*!
  * @class TVSEnvironment
+ * @warning 必须在调用 TVSAccountSDK 其他接口前设置！！
  * @brief TVS 环境设置接口
  */
 @interface TVSEnvironment : NSObject
@@ -19,6 +20,11 @@
  * @brief 是否测试环境
  */
 @property(nonatomic,assign) BOOL test;
+
+/*!
+ * @brief 是否开启日志
+ */
+@property(nonatomic,assign) BOOL logEnabled;
 
 /*!
  * @brief 获得 TVS 环境类单例对象
